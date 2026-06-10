898f8fe25875bf2b816735da8eaa920026807107
Ekaterina Tumanova <tumanova.ekaterina73@gmail.com>
2026-06-10 08:21:34 +0400

    описание изменений

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -30,7 +30,7 @@ class CookiesBanner {
 
                 <div class="cookies-banner__content">
                     <p class="cookies-banner__text">
-                        Продолжая пользоваться сайтом, вы соглашаетесь с использованием <a href="./privacy.html" class="cookies-banner__link">cookie-файлов</a> и обработкой обезличенных данных через Яндекс.Метрику согласно Политике конфиденциальности
+                        Мы используем <a href="./privacy.html" class="cookies-banner__link">cookie-файлы</a> для корректной работы сайта и анализа посещаемости
                     </p>
                 </div>
 
